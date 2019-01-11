e8223014a71e26cc392cf59c3cb0530a8cf548ba
Geenay <geenaylaubscher@hotmail.com>
2019-01-11 22:31:17 +0000

    new to-do

--- app.js
+++ app.js
@@ -1,23 +1,69 @@
 function onReady() {
-  const addToDoForm = document.getElementById('addToDoForm');
-    const newToDoText = document.getElementById('newToDoText');
-    const toDoList = document.getElementById('toDoList');
+  let toDos = JSON.parse(localStorage.getItem('toDos')) || [];
+  let id = toDos.length || 0;
+  const addToDoForm = document.getElementById('newToDoText');
 
-       addToDoForm.addEventListener('submit', () => {
-         event.preventDefault();
+  function createNewToDo(){
+    toDos.push({
+      title: newToDoText.value,
+      complete: false,
+      id: ++id
+    });
+  }
+  function deleteToDo(id){
+    return toDos.filter(toDo => toDO.id !== id);
+}
 
-           let title = newToDoText.value;
+function saveToDos() {
+  localStorage.seItem('toDos', JSON.stringify(toDos) );
+}
+function renderTheUI(){
+  const toDoList = document.getElementById('toDoList');
+  toDoList.textContent = '';
 
-           // create a new li
-    let newLi = document.createElement('li');
+  toDos.forEach(function(toDo){
+    const newLi = document.createElement('li');
 
-    // create a new input
-    let checkbox = document.createElement('input');
-
-    // set the input's type to checkbox
+    const checkbox = document.createElement('li');
     checkbox.type = "checkbox";
+    checkbox.checked = toDo.complete;
+
+    const DeleteBtn = document.createElement('Button')
+DeleteBtn.innerHTML = '<span>Delete</span>';
+
+newli.innerHTML = toDo.title;
+
+checkbox.addEventListener( 'click', function(){
+  toDo.complete = checkbox.checked ? true : false;
+  saveToDos();
 });
+
+newLi.appendChild(checkbox);
+toDoList.appendChild(newLi);
+newLi.appendChild(DeleteBtn);
+
+deleteBtn.addEventListener('click',() => {
+  toDos = deleteToDo(toDo.id);
+  renderTheUI();
+  saveToDos();
+
+});
+});
+}
+
+  addToDoForm.addEventListener('submit', function(event){
+       event.preventDefault();
+       createNewToDo();
+       newToDoText.value = '';
+       renderTheUI();
+       saveToDos();
+  });
+  renderTheUI();
+  saveToDos();
+}
+
+
+
 window.onload = function() {
-   alert("The window has loaded!");
    onReady();
  };
